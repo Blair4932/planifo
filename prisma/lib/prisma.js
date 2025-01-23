d94f2e20fbd6c9aeee815aa0d9d58a92282db5d6
Blair Scott <blairs4932@outlook.com>
2025-01-23 10:17:07 +0000

    changes

--- prisma/lib/prisma.js
+++ prisma/lib/prisma.js
@@ -6,11 +6,4 @@ const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-async function main() {
-  const user = await prisma.user.create({
-    data: { username: "test", password: "password", email: "test@manifo.uk" },
-  });
-}
-
-main();
 export default prisma;
