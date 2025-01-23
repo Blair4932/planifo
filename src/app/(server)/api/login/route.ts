98ec1e217b2b7d1febdee31700314a57901e65a2
Blair Scott <blairs4932@outlook.com>
2025-01-23 09:55:24 +0000

    Error logging

--- src/app/(server)/api/login/route.ts
+++ src/app/(server)/api/login/route.ts
@@ -4,13 +4,21 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export async function POST(req: Request) {
-  const { username, password } = await req.json();
-
   try {
+    const { username, password } = await req.json();
+
+    if (!username || !password) {
+      return NextResponse.json(
+        { error: "Username and password are required" },
+        { status: 400 }
+      );
+    }
+
     const user = await prisma.user.findUnique({
       where: { username },
       include: {
         notes: true,
+        tables: true,
       },
     });
 
@@ -22,7 +30,6 @@ export async function POST(req: Request) {
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
-
     if (!isPasswordValid) {
       return NextResponse.json(
         { error: "Invalid username or password" },
@@ -36,12 +43,17 @@ export async function POST(req: Request) {
         username: user.username,
         email: user.email,
         notes: user.notes,
+        tables: user.tables,
       },
       process.env.JWT_SECRET!,
       { expiresIn: "1d" }
     );
 
-    return NextResponse.json({ message: "Login successful", token });
+    // Return successful response
+    return NextResponse.json({
+      message: "Login successful",
+      token,
+    });
   } catch (error) {
     console.error("Error during login:", error);
     return NextResponse.json(
