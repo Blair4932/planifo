4e99e2da6e12aea02c49634916f85395547c1830
Blair Scott <blairs4932@outlook.com>
2025-02-16 09:04:03 +0000

    0.8-0.9

--- next.config.ts
+++ next.config.ts
@@ -4,18 +4,13 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizeCss: false,
   },
-  transpilePackages: ["swiper", "ssr-window", "dom7"],
+  poweredByHeader: false,
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: false,
+  },
   webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "swiper/css": require.resolve("swiper/swiper.min.css"),
-      "swiper/css/navigation": require.resolve(
-        "swiper/modules/navigation/navigation.min.css"
-      ),
-      "swiper/css/pagination": require.resolve(
-        "swiper/modules/pagination/pagination.min.css"
-      ),
-    };
+    return config;
   },
 
   async headers() {
