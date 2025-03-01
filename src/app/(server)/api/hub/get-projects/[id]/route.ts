1520e9213d1ae224d4d9405f20929082f8a12b0f
Blair4932 <blairs4932@outlook.com>
2025-03-01 16:19:12 +0000

    FLICKER

--- src/app/(server)/api/hub/get-projects/[id]/route.ts
+++ src/app/(server)/api/hub/get-projects/[id]/route.ts
@@ -1,8 +1,8 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import prisma from "@/prisma/lib/prisma";
 
 export async function GET(
-  req: Request,
+  req: NextRequest,
   { params }: { params: { id: string } }
 ) {
   if (!params.id) {
@@ -21,8 +21,6 @@ export async function GET(
         milestones: true,
       },
     });
-
-    console.log(project);
 
     if (!project) {
       return NextResponse.json({ error: "Project not found" }, { status: 404 });
